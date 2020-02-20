fee0bfb7bb76221752bb6d369a5d66bf8b92f48f
SunCe <ce.sun@tom.com>
2020-02-20 17:48:09 +0800

    设置服务端和客户端的OUT_PUT名称，和输出目录

---
+++ cmake-build-debug/CMakeFiles/Engine.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../build/Client/GameClient.exe"
+  "../build/Client/GameClient.exe.manifest"
+  "../build/Client/GameClient.pdb"
+  "../build/Client/libGameClient.dll.a"
+  "CMakeFiles/Engine.dir/src/Client/CCamera/CCamera.cpp.obj"
+  "CMakeFiles/Engine.dir/src/Client/CClient/CClient.cpp.obj"
+  "CMakeFiles/Engine.dir/src/Client/CDemo/CDemo.cpp.obj"
+  "CMakeFiles/Engine.dir/src/Client/CShader/CShader.cpp.obj"
+  "CMakeFiles/Engine.dir/src/Client/main.cpp.obj"
+  "CMakeFiles/Engine.dir/src/Server/CMessage/CMessage.cpp.obj"
+  "CMakeFiles/Engine.dir/src/Server/CServer/CServer.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Server/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by SunCe on 2020/2/20.
+//
+
+#include <iostream>
+
+int main() {
+    std:: cout << "Server Launch" << std::endl;
+    return 0;
+}
